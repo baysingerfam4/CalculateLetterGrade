039a9158f8d06a2a4cd1cd5efc71bfd0554eae7f
baysingerfam4 <165937206+baysingerfam4@users.noreply.github.com>
2024-09-21 23:35:03 -0700

    Add project files.

---
+++ frmCalculateGrade.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace CalculateLetterGrade
+{
+    partial class frmCalculateGrade
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtNumberGrade = new TextBox();
+            label2 = new Label();
+            txtLetterGrade = new TextBox();
+            btnCalculate = new Button();
+            btnExit = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(47, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(87, 15);
+            label1.TabIndex = 0;
+            label1.Text = "&Number grade:";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Click += label1_Click;
+            // 
+            // txtNumberGrade
+            // 
+            txtNumberGrade.Location = new Point(140, 46);
+            txtNumberGrade.Name = "txtNumberGrade";
+            txtNumberGrade.Size = new Size(114, 23);
+            txtNumberGrade.TabIndex = 1;
+            txtNumberGrade.TextChanged += textBox1_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(47, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Letter Grade:";
+            // 
+            // txtLetterGrade
+            // 
+            txtLetterGrade.Location = new Point(140, 75);
+            txtLetterGrade.Name = "txtLetterGrade";
+            txtLetterGrade.ReadOnly = true;
+            txtLetterGrade.Size = new Size(114, 23);
+            txtLetterGrade.TabIndex = 3;
+            txtLetterGrade.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            btnCalculate.Location = new Point(46, 121);
+            btnCalculate.Name = "btnCalculate";
+            btnCalculate.Size = new Size(75, 23);
+            btnCalculate.TabIndex = 2;
+            btnCalculate.Text = "&Calculate";
+            btnCalculate.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(179, 121);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(75, 23);
+            btnExit.TabIndex = 3;
+            btnExit.Text = "E&xit";
+            btnExit.UseVisualStyleBackColor = true;
+            // 
+            // form1
+            // 
+            AcceptButton = btnCalculate;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnExit;
+            ClientSize = new Size(322, 185);
+            Controls.Add(btnExit);
+            Controls.Add(btnCalculate);
+            Controls.Add(txtLetterGrade);
+            Controls.Add(label2);
+            Controls.Add(txtNumberGrade);
+            Controls.Add(label1);
+            Name = "form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculate Letter Grade";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNumberGrade;
+        private Label label2;
+        private TextBox txtLetterGrade;
+        private Button btnCalculate;
+        private Button btnExit;
+    }
+}
---
+++ frmCalculateGrade.cs
@@ -0,0 +1,25 @@
+namespace CalculateLetterGrade
+{
+    public partial class frmCalculateGrade : Form
+    {
+        public frmCalculateGrade()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
